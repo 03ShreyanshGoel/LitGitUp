c70035b239629c44d6141c870984e9ca5a1c72fa
shreyansh goel <03shreyanshgoel@gmail.com>
2025-02-07 00:26:02 +0530

    added issues list page

--- src/app/(protected)/meetings/page.tsx
+++ src/app/(protected)/meetings/page.tsx
@@ -52,7 +52,6 @@ const MeetingsPage = () => {
                                 }
                             })}>Delete Meeting</Button>
                         </div>
-
                     </li>
                 ))}
             </ul>
